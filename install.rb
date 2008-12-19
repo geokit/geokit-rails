8fdfd1f524c25797d4bdf40b73ffd8f09cc64e14
Andre Lewis <andre@highgroove.com>
2008-12-18 19:26:38 -0800

    fixed config file creation in install.rb

--- install.rb
+++ install.rb
@@ -7,7 +7,7 @@ template_path=File.join(File.dirname(__FILE__), '/assets/api_keys_template')
 if File.exists?(path)
   puts "It looks like you already have a configuration file at #{path}. We've left it as-is. Recommended: check #{template_path} to see if anything has changed, and update config file accordingly."
 else
-  File.open(path) do |f|
+  File.open(path, "w") do |f|
     f.puts IO.read(template_path)
     puts "We created a configuration file for you in config/initializers/geokit_config.rb. Add your Google API keys, etc. there."
   end  
