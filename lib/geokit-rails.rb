c1d3f7514f06b1962537aaf509cc014050b8f65c
Andre Lewis <andre@highgroove.com>
2009-04-11 02:32:08 -0700

    updated changelong and added message if gem can't be found

--- lib/geokit-rails.rb
+++ lib/geokit-rails.rb
@@ -2,8 +2,8 @@
 # ActionController helpers.  All other functionality must be explicitly 
 # required.
 #
-# Note that we don't explicitly require the geokit gem. You should specify gem dependencies in your config/environment.rb
-#  with this line:   config.gem "andre-geokit", :lib=>'geokit', :source => 'http://gems.github.com'
+# Note that we don't explicitly require the geokit gem. 
+# You should specify gem dependencies in your config/environment.rb: config.gem "geokit"
 #
 if defined? Geokit
   require 'geokit-rails/defaults'
@@ -15,4 +15,11 @@ if defined? Geokit
   
   # Automatically mix in ip geocoding helpers into ActionController classes.
   ActionController::Base.send :include, GeoKit::IpGeocodeLookup
+else
+  message=%q(WARNING: geokit-rails requires the Geokit gem. You either don't have the gem installed,
+or you haven't told Rails to require it. If you're using a recent version of Rails: 
+  config.gem "geokit" # in config/environment.rb
+and of course install the gem: sudo gem install geokit)
+  puts message
+  Rails.logger.error message
 end
