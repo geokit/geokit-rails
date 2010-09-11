ddd8f4172f31eb1d913dd78b2cd15175d59248b6
Jeremy Lecour <jeremy.lecour@gmail.com>
2010-09-11 15:50:32 +0200

    Rename the gem to geokit_rails
    It will be easier to have some config keys, … if tere is no dash in the name.
    There is still a lib/geokit-rails.rb for "historic" compatibility (is it really needed ?)

--- lib/geokit-rails.rb
+++ lib/geokit-rails.rb
@@ -1,5 +1,1 @@
-require 'geokit'
-
-require 'geokit-rails/railtie'
-require 'geokit-rails/core_extensions'
-require 'geokit-rails/acts_as_mappable'
+require 'geokit_rails'
---
+++ lib/geokit_rails.rb
@@ -0,0 +1,5 @@
+require 'geokit'
+
+require 'geokit_rails/railtie'
+require 'geokit_rails/core_extensions'
+require 'geokit_rails/acts_as_mappable'
