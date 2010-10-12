6de1f84c9a75a5c314fb4cdaa917adb79fb88ac6
Jeremy Lecour <jeremy.lecour@gmail.com>
2010-10-12 07:41:37 +0200

    Fix: forgot to rename the main lib file

--- lib/geokit-rails3/railtie.rb
+++ lib/geokit-rails3/railtie.rb
@@ -4,28 +4,28 @@ require 'rails'
 module Geokit
 
   class Railtie < Rails::Railtie
-    
+
     config.geokit = ActiveSupport::OrderedOptions.new
     config.geokit.geocoders = ActiveSupport::OrderedOptions.new
-    
+
     initializer 'geokit-rails3.insert_into_active_record' do
       ActiveSupport.on_load :active_record do
         ActiveRecord::Base.send(:include, Geokit::ActsAsMappable::Glue)
         Geokit::Geocoders.logger = ActiveRecord::Base.logger
       end
     end
-    
+
     initializer 'geokit-rails3.insert_into_action_controller' do
       ActiveSupport.on_load :action_controller do
         ActionController::Base.send(:include, Geokit::GeocoderControl)
         ActionController::Base.send(:include, GeoKit::IpGeocodeLookup)
       end
     end
-    
+
     config.after_initialize do |app|
       options = app.config.geokit
       geocoders_options = options.delete(:geocoders)
-      
+
       options.each do |k,v|
         Geokit::send("#{k}=", v)
       end
@@ -34,5 +34,5 @@ module Geokit
       end
     end
   end
-  
+
 end
