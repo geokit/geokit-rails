1df71db199c2065dafa6bae53f1975ddb34020b6
streetpc <streetpc@free.fr>
2011-04-20 02:54:59 -0700

    Added NULL lat/lng handling for SQLite

--- lib/geokit-rails3/adapters/sqlite.rb
+++ lib/geokit-rails3/adapters/sqlite.rb
@@ -31,16 +31,20 @@ module Geokit
       
       def sphere_distance_sql(lat, lng, multiplier)
         %|
+          (CASE WHEN #{qualified_lat_column_name} IS NULL OR #{qualified_lng_column_name} IS NULL THEN NULL ELSE
           (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+
           COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+
           SIN(#{lat})*SIN(RADIANS(#{qualified_lat_column_name}))))*#{multiplier})
+          END)
          |
       end
       
       def flat_distance_sql(origin, lat_degree_units, lng_degree_units)
         %|
+          (CASE WHEN #{qualified_lat_column_name} IS NULL OR #{qualified_lng_column_name} IS NULL THEN NULL ELSE
           SQRT(POW(#{lat_degree_units}*(#{origin.lat}-#{qualified_lat_column_name}),2)+
           POW(#{lng_degree_units}*(#{origin.lng}-#{qualified_lng_column_name}),2))
+          END)
          |
       end
       
