8112af00c3d4230dc64e6db68619d5f2d45fd93e
Andy Rusterholz <andy@blinker.com>
2015-07-27 10:49:05 -0600

    Check to see if adapter is loaded before requiring a file

--- lib/geokit-rails/acts_as_mappable.rb
+++ lib/geokit-rails/acts_as_mappable.rb
@@ -93,7 +93,9 @@ module Geokit
       # A proxy to an instance of a finder adapter, inferred from the connection's adapter.
       def adapter
         @adapter ||= begin
-          require File.join('geokit-rails', 'adapters', connection.adapter_name.downcase)
+          unless Adapters.const_defined?(connection.adapter_name.camelcase)
+            require File.join('geokit-rails', 'adapters', connection.adapter_name.downcase)
+          end
           klass = Adapters.const_get(connection.adapter_name.camelcase)
           klass.load(self) unless klass.loaded
           klass.new(self)
