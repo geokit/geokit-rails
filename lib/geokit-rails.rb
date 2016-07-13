617cdaf9eff83845d8db87cc31cf0fb6cb08d422
Michael Noack <michael@noack.com.au>
2016-07-13 23:45:38 +0930

    Remove duplicate file that exists in geokit

--- lib/geokit-rails.rb
+++ lib/geokit-rails.rb
@@ -3,7 +3,6 @@ require 'geokit'
 require 'geokit-rails/railtie'
 require 'geokit-rails/core_extensions'
 
-require 'geokit-rails/defaults'
 require 'geokit-rails/adapters/abstract'
 require 'geokit-rails/acts_as_mappable'
 require 'geokit-rails/geocoder_control'
