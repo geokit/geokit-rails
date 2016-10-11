767e2ec9fafd2f4e560c3c4d42f61cfe1ce02cfe
Michael Noack <michael@noack.com.au>
2016-10-11 20:49:54 +1030

    Remove redundant self and spacing

--- lib/geokit-rails/geocoder_control.rb
+++ lib/geokit-rails/geocoder_control.rb
@@ -3,15 +3,15 @@ require 'active_support/concern'
 module Geokit
   module GeocoderControl
     extend ActiveSupport::Concern
-  
+
     included do
-      if self.respond_to? :before_action
-        self.send :before_action, :set_geokit_domain
-      elsif self.respond_to? :before_filter
-        self.send :before_filter, :set_geokit_domain
+      if respond_to? :before_action
+        send :before_action, :set_geokit_domain
+      elsif respond_to? :before_filter
+        send :before_filter, :set_geokit_domain
       end
     end
-    
+
     def set_geokit_domain
       Geokit::Geocoders::domain = request.domain
     end
