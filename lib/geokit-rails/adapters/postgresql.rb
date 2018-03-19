71bbb643bd2f827c3c021e13c3ee759b4a205670
Diego Monteiro <dmsburn@gmail.com>
2018-03-19 12:29:55 -0300

    Setup adapter postgresql to float calculations

--- lib/geokit-rails/adapters/postgresql.rb
+++ lib/geokit-rails/adapters/postgresql.rb
@@ -3,6 +3,11 @@ module Geokit
     class PostgreSQL < Abstract
       
       def sphere_distance_sql(lat, lng, multiplier)
+        lat = "CAST(#{lat} AS FLOAT)"
+        lng = "CAST(#{lng} AS FLOAT)"
+        qualified_lat_column_name = "CAST(#{qualified_lat_column_name} AS FLOAT)"
+        qualified_lng_column_name = "CAST(#{qualified_lng_column_name} AS FLOAT)"
+
         %|
           (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+
           COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+
@@ -11,6 +16,10 @@ module Geokit
       end
       
       def flat_distance_sql(origin, lat_degree_units, lng_degree_units)
+        lat_degree_units = "CAST(#{lat_degree_units} AS FLOAT)"
+        lng_degree_units = "CAST(#{lng_degree_units} AS FLOAT)"
+        qualified_lat_column_name = "CAST(#{qualified_lat_column_name} AS FLOAT)"
+        qualified_lng_column_name = "CAST(#{qualified_lng_column_name} AS FLOAT)"
         %|
           SQRT(POW(#{lat_degree_units}*(#{origin.lat}-#{qualified_lat_column_name}),2)+
           POW(#{lng_degree_units}*(#{origin.lng}-#{qualified_lng_column_name}),2))
