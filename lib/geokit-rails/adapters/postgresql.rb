3549f0bc3591226ff37b0ee46fe6a7a5c84d6dd6
Diego Monteiro <dmsburn@gmail.com>
2018-03-19 13:16:05 -0300

    Fixing errors

--- lib/geokit-rails/adapters/postgresql.rb
+++ lib/geokit-rails/adapters/postgresql.rb
@@ -5,8 +5,8 @@ module Geokit
       def sphere_distance_sql(lat, lng, multiplier)
         lat = "CAST(#{lat} AS FLOAT)"
         lng = "CAST(#{lng} AS FLOAT)"
-        qualified_lat_column_name = "CAST(#{qualified_lat_column_name} AS FLOAT)"
-        qualified_lng_column_name = "CAST(#{qualified_lng_column_name} AS FLOAT)"
+        qualified_lat_column_name = "CAST(#{qualified_lat_column_name} AS FLOAT)" unless qualified_lat_column_name.nil?
+        qualified_lng_column_name = "CAST(#{qualified_lng_column_name} AS FLOAT)" unless qualified_lng_column_name.nil?
         %|
           (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+
           COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+
